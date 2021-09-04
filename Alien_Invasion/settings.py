66852a0c6b6b7ca3e73c046fda5f67f4f4ef58fb
Nandor Hegyi <hegyinandor1996@gmail.com>
2021-09-04 18:11:05 +0100

    add play button

--- Alien_Invasion/settings.py
+++ Alien_Invasion/settings.py
@@ -14,7 +14,7 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 1.5
-        self.bullet_width = 3
+        self.bullet_width = 30
         self.bullet_height = 15
         self.bullet_colour = (60, 60, 60)
         self.bullets_allowed = 3
